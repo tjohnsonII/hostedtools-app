0b77780c1b447196500753647315e171de504a67
tjohnsonII <tjohnson082083@gmail.com>
2025-07-25 17:25:43 +0000

    update

--- frontend/build.sh
+++ frontend/build.sh
@@ -3,6 +3,8 @@
 npm install
 npm run build
 
-sudo rm /var/www/html/index.html   # remove default page
+if [ -f /var/www/html/index.html ]; then
+    sudo rm /var/www/html/index.html   # remove default page
+fi
 sudo cp -r /home/tim2/hostedtools-app/frontend/build/* /var/www/html/  # copy new build files
 sudo chown -R www-data:www-data /var/www/html  # ensure correct permissions
--- start-node-proxy-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the TypeScript Node.js proxy server
-echo "Starting TypeScript Node.js proxy server..."
-cd /home/tim2/hostedtools-app/backend  # Correct backend directory
-npm install  # Ensure dependencies are installed
-npm run build  # Build the backend
-npm start  # Start the backend server
-npm test  # Run backend tests
